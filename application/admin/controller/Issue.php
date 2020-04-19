e2ada6cc801382c8b317a644fcdca466b1f0998b
wangyudi0107 <602230871@qq.com>
2020-04-19 12:31:37 +0800

    v1.1版本

--- application/admin/controller/Issue.php
+++ application/admin/controller/Issue.php
@@ -9,7 +9,11 @@ class Issue extends Controller{
 		$Common -> lin_in();//验证登录
 		$Common -> indexes($a,$b);//传入索引
 	}
-	
+//	+——————————————————————————
+//	|  首页
+//	+——————————————————————————
+
+	// 首页显示所有题目
 	public function index(){
 		//导入索引，验证登录
 		$this -> verify(4,0);
@@ -18,13 +22,14 @@ class Issue extends Controller{
 			$where['issue'] = array('like','%'.input('tittle').'%');
 		}
 		
-		$result = db('issue_list') -> where($where) -> paginate(10,false,['query'=>request()->param()]);
+		$result = db('issue_list') -> where($where) -> order('id esc') -> paginate(10,false,['query'=>request()->param()]);
 		$page = $result -> render();  //获取分页
 		$this -> assign('intro',$result);
 		$this -> assign('page',$page);
 		return view();
 	}
 	
+	// 添加题目接口
 	public function issue_add(){
 		$result = db('issue_list') -> insert(input());
 		if($result){
@@ -34,12 +39,41 @@ class Issue extends Controller{
 		}
 	}
 	
-	//问题信息删除
+	// 修改题目信息提交接口
+	public function issue_change_commit(){
+		$data['issue'] = input('issue');
+		$data['option'] = input('option');
+		$data['true_option'] = input('true_option');
+		$result = db('issue_list') ->where('id',input('id')) -> update($data);
+		if($result){
+			echo json_encode(array('status'=>1,"msg"=> '修改成功'));exit;
+		}else{
+			echo json_encode(array('status'=>2,"msg"=> '修改失败'));exit;
+		}
+	}
+	
+	// 题目信息删除接口
 	public function issue_del(){
 		db('issue_list') -> where('id',input('id')) -> delete();
 		echo json_encode(array('status'=>1,"msg"=> '删除成功'));exit;
 	}
 	
+	// 题目信息修改信息获取接口
+	public function issue_edit(){
+		$result = db('issue_list') -> where('id',input('id')) -> find();
+		$result['option'] = explode(',',$result['option']);
+		array_pop($result['option']);
+		for($i=0;$i<count($result['option']);$i++){
+			$result['option'][$i] = explode('. ',$result['option'][$i]);
+		}
+		echo json_encode($result);exit;
+	}
+	
+//	+——————————————————————————
+//	|  用户答题信息页面
+//	+——————————————————————————
+	
+	// 用户答题信息赋值
 	public function issue_record(){
 		//导入索引，验证登录
 		$this -> verify(4,1);
@@ -51,31 +85,70 @@ class Issue extends Controller{
 			$where['phone'] = array('like','%'.input('tittle').'%');
 		}
 		
-		$result = db('issue_user') -> where($where) -> paginate(10,false,['query'=>request()->param()]);
+		$result = db('issue_user') -> where($where) -> order('id esc') -> paginate(10,false,['query'=>request()->param()]);
 		$page = $result -> render();  //获取分页
 		$this -> assign('intro',$result);
 		$this -> assign('page',$page);
 		return view();
 	}
 	
-	//问题信息删除
+	//用户答题信息删除接口
 	public function user_del(){
 		db('issue_user') -> where('id',input('id')) -> delete();
 		echo json_encode(array('status'=>1,"msg"=> '删除成功'));exit;
 	}
 	
+//	+——————————————————————————
+//	|  红包信息页面
+//	+——————————————————————————
+
+	// 红包信息赋值
 	public function red_pack_set(){
 		//导入索引，验证登录
 		$this -> verify(4,2);
-		$result = db('issue_red_pack') -> where($where) ->select();
+		$result = db('issue_red_pack') -> where($where) -> order('id esc') ->select();
 		$this -> assign('intro',$result);
 		
 		return view();
 	}
 	
-	//红包信息删除
+	// 红包信息删除
 	public function pack_del(){
 		db('issue_red_pack') -> where('id',input('id')) -> delete();
 		echo json_encode(array('status'=>1,"msg"=> '删除成功'));exit;
 	}
+	
+	// 添加红包接口
+	public function red_pack_add(){
+		$result = db('issue_red_pack') -> insert(input());
+		if($result){
+			echo json_encode(array('status'=>1,"msg"=> '添加成功'));exit;
+		}else{
+			echo json_encode(array('status'=>2,"msg"=> '添加失败'));exit;
+		}
+	}
+	
+	// 题目信息修改信息获取接口
+	public function red_pack_edit(){
+		$result = db('issue_red_pack') -> where('id',input('id')) -> find();
+		echo json_encode($result);exit;
+	}
+	
+	// 红包信息提交接口
+	public function red_pack_change_commit(){
+		$data['name'] = input('name');
+		$data['money'] = input('money');
+		$data['num'] = input('num');
+		$data['residual_amount'] = input('residual_amount');
+		$data['residual_num'] = input('residual_num');
+		$data['status'] = input('status');
+		$result = db('issue_red_pack') ->where('id',input('id')) -> update($data);
+		if($result){
+			echo json_encode(array('status'=>1,"msg"=> '修改成功'));exit;
+		}else{
+			echo json_encode(array('status'=>2,"msg"=> '修改失败'));exit;
+		}
+	}
+	
+	
 }
